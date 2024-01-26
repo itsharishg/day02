f4714ecae0e99ec1fee22432273ac5166e07bc66
itsharishg <itsharishg@gmail.com>
2024-01-26 13:49:54 +0530

    day03 tasks

--- script.js
+++ script.js
@@ -4,9 +4,7 @@
 
 var obj1 = {"name":"Person1","age":5};
 var obj2 = {"age":5,"name":"Person1"};
-
 var flag=true;
-
 if(Object.keys(obj1).length==Object.keys(obj2).length){
     for(key in obj1) { 
         if(obj1[key] == obj2[key]) {
