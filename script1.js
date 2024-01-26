b3f6c28a818047823fac4d07fb60cd5000c84605
itsharishg <itsharishg@gmail.com>
2024-01-26 13:52:18 +0530

    day03 tasks

--- script1.js
+++ script1.js
@@ -7,10 +7,10 @@ request.send();
 request.onload = function(){
     var res = JSON.parse(request.response);
     console.log(res);
-// Question 2 Solution
+// Question 2 Solution ( Displayed all the country flags in the console )
 for(var i =0;i<res.length;i++){
     console.log(res[i].flags.png)
-// Question 3 Solution
+// Question 3 Solution ( Printed all countries names, regions, sub regions and populations )
     console.log(res[i].name.common+" "+res[i].region+" "+res[i].subregion+" "+res[i].population);
 }
 }
