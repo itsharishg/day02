4cc156bf4939785d1c31a303becd73f4b939f32c
itsharishg <itsharishg@gmail.com>
2024-01-26 13:29:28 +0530

    day03 tasks

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 // a. let obj1 = {name:"Person1", age:5};
 // b. let obj2 = {age:5,name: "Person1"};
 
+var
+remoteJSON = {"allowExternalMembers": "false", "whoCanJoin": "CAN_REQUEST_TO_JOIN"},
+    localJSON = {"whoCanJoin": "CAN_REQUEST_TO_JOIN", "allowExternalMembers": "false"};
+    
+console.log( _.isEqual(remoteJSON, localJSON) );
