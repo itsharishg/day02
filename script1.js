c5d9b203dc986a5b6098bb6eebf40f6272da1ce2
itsharishg <itsharishg@gmail.com>
2024-01-26 13:22:54 +0530

    day03 tasks

---
+++ script1.js
@@ -0,0 +1,16 @@
+// 3. Use the same rest countries and 
+// print all countries names, regions, sub-region and populations
+
+var request= new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all")
+request.send();
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    console.log(res);
+// Question 2 Solution
+for(var i =0;i<res.length;i++){
+    console.log(res[i].flags.png)
+// Question 3 Solution
+    console.log(res[i].name.common+" "+res[i].region+" "+res[i].subregion+" "+res[i].population);
+}
+}
